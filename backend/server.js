9a803b3f7ae9486228523d120651af0bbbfd0a72
unitax13 <61455951+unitax13@users.noreply.github.com>
2022-04-20 22:15:30 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,16 @@ app.get('/', (req,res) => res.status(200).json({message: 'Hello, / (main page)'}
 app.use('/api/memos', require('./routes/memoRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
+//serve frontend
+if (process.env.NODE_ENV === 'production') {
+    console.log("Starting in production...")
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'frontend','build','index.html'))
+    })
+}
+
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
